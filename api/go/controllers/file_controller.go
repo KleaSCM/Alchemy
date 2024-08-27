b2fd13ef5da0d80fe7da8c88ee877abc3ebebf4e
Your Name <john.farmer1086@gmail.com>
2024-08-27 11:14:54 +0800

    list files

--- api/go/controllers/file_controller.go
+++ api/go/controllers/file_controller.go
@@ -17,12 +17,43 @@ func init() {
 	}
 }
 
+//upload files
 func UploadFile(c *gin.Context) {
 	file, _, err := c.Request.FormFile("file")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to upload file"})
 		return
 	}
+
+// to delete files
+func DeleteFile(c *gin.Context) {
+	fileName := c.Param("name")
+	filePath := filepath.Join(uploadDir, fileName)
+
+	if err := os.Remove(filePath); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "File deleted successfully"})
+}
+
+//list files
+func ListFiles(c *gin.Context) {
+	files, err := os.ReadDir(uploadDir)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to list files"})
+		return
+	}
+
+	var fileNames []string
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+
+	c.JSON(http.StatusOK, gin.H{"files": fileNames})
+}
+
 	defer file.Close()
 
 	// Save file to disk
