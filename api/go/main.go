595bd9d203b743c50dcfcc7b8a4cbf7357df9b35
Your Name <john.farmer1086@gmail.com>
2024-08-27 10:50:50 +0800

    expand the /metadata route to extract metadata such as file size, creation date, and modification date

--- api/go/main.go
+++ api/go/main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
-	"github.com/Jay-SCM/alchemy/api/go/controllers"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+type Metadata struct {
+	Name         string    `json:"name"`
+	Size         int64     `json:"size"`
+	CreationTime time.Time `json:"creation_time"`
+	ModTime      time.Time `json:"mod_time"`
+}
+
+type MetadataResponse struct {
+	Files []Metadata `json:"files"`
+}
+
+func extractMetadata(directory string) ([]Metadata, error) {
+	var metadataList []Metadata
+
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			metadata := Metadata{
+				Name:         info.Name(),
+				Size:         info.Size(),
+				CreationTime: info.ModTime(), // Assuming ModTime as creation time as Go doesn't expose actual creation time on all platforms
+				ModTime:      info.ModTime(),
+			}
+			metadataList.append(metadata)
+		}
+		return nil
+	})
+
+	return metadataList, err
+}
 
 func main() {
 	router := gin.Default()
 
-	// Connect routes to their handler functions
-	router.GET("/files", controllers.ListFiles)
-	router.POST("/upload", controllers.UploadFile)
-	router.DELETE("/delete/:filename", controllers.DeleteFile)
-	router.GET("/metadata/:filename", controllers.GetFileMetadataHandler)
+	// Metadata Extractor route
+	router.GET("/metadata", func(c *gin.Context) {
+		directory := c.Query("directory")
+		if directory == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Directory path is required"})
+			return
+		}
+		metadataList, err := extractMetadata(directory)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to extract metadata"})
+			return
+		}
+		c.JSON(http.StatusOK, MetadataResponse{Files: metadataList})
+	})
 
-	router.Run(":8080")
+	router.Run(":8080") // Start the server on port 8080
 }
